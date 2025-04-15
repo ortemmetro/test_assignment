eb2051efab999ef140bbbd7ffc1e863e4471af61
Артём Руппель <ortem.koldun@gmail.com>
2025-04-15 17:25:49 +0500

    some changes

--- lib/src/di/modules/constants_module.dart
+++ lib/src/di/modules/constants_module.dart
@@ -3,7 +3,8 @@ import 'package:get_it/get_it.dart';
 abstract class ConstantsModule {
   static Future<void> registerInstances(GetIt getIt) async {
     getIt.registerFactory<String>(
-      () => 'AIzaSyA76nsmDXvYl3xd_3EUEIqwyNigOj-9w_M',
+      () =>
+          'insert_here_your_google_maps_api_key', // Insert your Google Maps API key here
       instanceName: 'google_map_api_key',
     );
   }
